9a0acd0e0ace3f7c90a20ff118a68abc887a515a
Alec Doran-Twyford <alec@alectronic.co>
2018-02-26 09:47:29 +0000

    CI changes

--- buildrun-docker-base.sh
+++ buildrun-docker-base.sh
@@ -4,16 +4,16 @@ clear;
 ./bin/fakeversion.sh
 docker-compose down
 
-docker-compose  -f ./docker/compose/docker-compose-dm.yml \
--f ./docker/compose/docker-compose-dm-ports.yml \
+docker-compose  -f ./docker/compose/docker-compose-em.yml \
+-f ./docker/compose/docker-compose-em-ports.yml \
 -f ./docker/docker-compose-base.yml \
 -f ./docker/docker-compose-base-ports.yml \
 -f ./docker/compose/docker-compose-idam.yml \
 -f ./docker/compose/docker-compose-idam-ports.yml \
 pull
 
-docker-compose -f ./docker/compose/docker-compose-dm.yml \
--f ./docker/compose/docker-compose-dm-ports.yml \
+docker-compose -f ./docker/compose/docker-compose-em.yml \
+-f ./docker/compose/docker-compose-em-ports.yml \
 -f ./docker/docker-compose-base.yml \
 -f ./docker/docker-compose-base-ports.yml \
 -f ./docker/compose/docker-compose-idam.yml \
