936ff1fe8a27103f2bb304a87eb5116f77bfe6e9
Alec Doran-Twyford <alec@alectronic.co>
2018-02-27 16:36:33 +0000

    config endpoint added

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const config = require('config');
 let http = require("http");
 let app = express();
 
@@ -9,6 +10,11 @@ app.get("/health", (req, res) => {
   res.send({
     status: "UP"
   });
+});
+
+
+app.get("/config", (req, res) => {
+  res.send(config);
 });
 
 const port = process.env.PORT || "3608";
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,10 @@ import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 import {HttpClient} from '@angular/common/http';
+import { environment } from '../environments/environment';
 
 @Injectable()
 export class AppConfig {
-
-  public static readonly CONFIG_PATH = '/assets/config.json';
 
   private config: Config;
 
@@ -16,9 +15,11 @@ export class AppConfig {
   public load(): Promise<void> {
     console.log('Loading app config...');
 
+    const configUrl = environment.configUrl;
+
     return new Promise<void>((resolve, reject) => {
       this.http
-        .get(AppConfig.CONFIG_PATH)
+        .get(configUrl)
         .subscribe((config: Config) => {
           this.config = config;
           console.log('Loading app config: OK');
