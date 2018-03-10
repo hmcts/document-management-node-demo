60d540c2aee9ca945b5eaa0acd5256aeba95d535
Alec Doran-Twyford <alec@alectronic.co>
2018-03-10 12:45:34 +0000

    missing package added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
+import {BrowserModule} from '@angular/platform-browser';
 import {APP_INITIALIZER, NgModule} from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import {HttpClientModule} from '@angular/common/http';
 import {RouterModule, Routes} from '@angular/router';
-import { FormsModule } from '@angular/forms';
-import {HttpModule} from '@angular/http';
+import {FormsModule} from '@angular/forms';
 import {CookieModule} from 'ngx-cookie';
 
 import {IdamGuard} from './auth/idam.guard';
 
-import { AppComponent } from './app.component';
-import { DmListViewComponent } from './dm-listview/dm-listview.component';
-import { DmListViewRouteComponent } from './dm-listview/dm-listview-route.component';
-import { DmUploadComponent } from './dm-upload/dm-upload.component';
-import { DmUploadRouteComponent } from './dm-upload/dm-upload-route.component';
+import {AppComponent} from './app.component';
+import {DmListViewComponent} from './dm-listview/dm-listview.component';
+import {DmListViewRouteComponent} from './dm-listview/dm-listview-route.component';
+import {DmUploadComponent} from './dm-upload/dm-upload.component';
+import {DmUploadRouteComponent } from './dm-upload/dm-upload-route.component';
 
+import {SessionService} from './auth/session.service';
 import {AppConfig} from './config/app.config';
 import {WindowService} from './utils/window.service';
-import {SessionService} from './auth/session.service';
 import {DocumentService} from './utils/document.service';
 import {DocumentStoreService} from './dm/document-store.service';
-import {EmAnnotationSummaryComponent, EmAnnotationSummaryModule, EmViewerComponent, EmViewerModule} from 'em-viewer-web';
+import {EmAnnotationSummaryModule, EmViewerModule} from 'em-viewer-web';
 import {EmViewerRouteComponent} from './em/em-viewer-route.component';
 import {EmAnnotationSummaryRouteComponent} from './em/em-annotation-summary.component';
 
@@ -53,7 +52,6 @@ const appRoutes: Routes = [
     HttpClientModule,
     EmViewerModule,
     EmAnnotationSummaryModule,
-    HttpModule,
     CookieModule.forRoot()
   ],
   providers: [
