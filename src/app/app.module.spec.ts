86e8d51178ac86ca31daa6183169d3bb97f33f2a
Louis Houghton <louis@louishoughton.com>
2018-03-12 16:13:58 +0000

    Basic test for module

---
+++ src/app/app.module.spec.ts
@@ -0,0 +1,26 @@
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {AppModule} from './app.module';
+import {AppComponent} from './app.component';
+import {APP_BASE_HREF} from "@angular/common";
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+
+
+describe('DmUploadComponent tests', () => {
+  let component;
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule, AppModule],
+      declarations: [],
+      providers: [{provide: APP_BASE_HREF, useValue : '/' }]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    component = TestBed.createComponent(AppComponent);
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
