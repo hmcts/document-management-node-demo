d0e2fc223258fed9cfbf105a35f393f075a7b523
Alec Doran-Twyford <alec@alectronic.co>
2018-03-12 18:14:50 +0000

    add express logging and hpm logging to hmcts standard (1.4.6)

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-const express = require("express");
+const express = require('express');
 const config = require('config');
 
-// const { Logger } = require('@hmcts/nodejs-logging');
+// const { Logger, Express } = require('@hmcts/nodejs-logging'); // 2.2.0 version
+// const logging = require('@hmcts/nodejs-logging'); // 1.4.6 version
+const { logging } = require('./node/logging/dm-logger'); // 1.4.6 version + our internal flavour
 const healthcheck = require('@hmcts/nodejs-healthcheck');
 const { InfoContributor, infoRequestHandler  } = require('@hmcts/info-provider');
 
@@ -13,29 +15,36 @@ let app = express();
 let http = require("http");
 let proxy = require('http-proxy-middleware');
 
+// Logger.config(config.get('logging')); // 2.2.0 version
+// app.use(Express.accessLogger()); // 2.2.0 version
+logging.config(config.get('logging')); // 1.4.6 version
+app.use(logging.express.accessLogger()); // 1.4.6 version
+
 app.use(cookieParser());
 app.use('/demproxy', authCheckerUserOnlyFilter);
 app.use('/demproxy', serviceFilter);
 
 app.use('/demproxy', proxy({
-  target: config.dm_store_app_url,
-  logLevel: 'debug',
-  router: {
-    '/demproxy/dm': config.get('dm_store_app_url'),
-    '/demproxy/an': config.get('em_anno_app_url'),
-  },
-  pathRewrite: {
-    '^/demproxy/dm': '/',
-    '^/demproxy/an': '/',
-  },
-  onProxyReq: (proxyReq, req, res) => {
-    if (!req.headers['ServiceAuthorization'] ||
-      !req.headers['user-id'] ||
-      !req.headers['user-roles']) {
-      throw new Error("Missing DEM required headers")
-    }
+    target: config.get('dm_store_app_url'),
+    logLevel: 'debug',
+    router: {
+      '/demproxy/dm': config.get('dm_store_app_url'),
+      '/demproxy/an': config.get('em_anno_app_url'),
+    },
+    pathRewrite: {
+      '^/demproxy/dm': '/',
+      '^/demproxy/an': '/',
+    },
+    onProxyReq: (proxyReq, req, res) => {
+      if (!req.headers['ServiceAuthorization'] ||
+        !req.headers['user-id'] ||
+        !req.headers['user-roles']) {
+        throw new Error("Missing DEM required headers")
+      }
+    },
+    logProvider: () => logging.getLogger('HPM')
   }
-}));
+));
 
 app.use('',express.static('dist'));
 app.use('/list',express.static('dist'));
@@ -55,22 +64,24 @@ app.get("/health", healthcheck.configure({
     'idam' : healthcheck.web(config.get('idam').get('base_url') + "/health"),
     'idamS2S' : healthcheck.web(config.get('idam').get('s2s_url') + "/health")
   },
-  buildInfo: {}
+  buildInfo: {
+
+  }
 }));
 
 app.get('/info', infoRequestHandler({
-    info: {
-      'dmStoreApp' :new InfoContributor(config.get('dm_store_app_url') + "/info"),
-      'emAnnoApp' : new InfoContributor(config.get('em_anno_app_url') + "/info"),
-      // 'emRedactApp' : new InfoContributor(config.get('em_redact_app_url') + "/info"),
-      'idam' : new InfoContributor(config.get('idam').get('base_url') + "/info"),
-      'idamS2S' : new InfoContributor(config.get('idam').get('s2s_url') + "/info")
-    },
-    extraBuildInfo: {
-      // featureToggles: config.get('featureToggles'),
-      // hostname: hostname()
-    }
-  }));
+  info: {
+    'dmStoreApp' :new InfoContributor(config.get('dm_store_app_url') + "/info"),
+    'emAnnoApp' : new InfoContributor(config.get('em_anno_app_url') + "/info"),
+    // 'emRedactApp' : new InfoContributor(config.get('em_redact_app_url') + "/info"),
+    'idam' : new InfoContributor(config.get('idam').get('base_url') + "/info"),
+    'idamS2S' : new InfoContributor(config.get('idam').get('s2s_url') + "/info")
+  },
+  extraBuildInfo: {
+    // featureToggles: config.get('featureToggles'),
+    // hostname: hostname()
+  }
+}));
 
 app.use('/demproxy', (err, req, res, next) => {
   res.contentType('application/json');
