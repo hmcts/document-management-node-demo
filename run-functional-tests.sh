591facfae344bbd6b79c967bc895c9f757af8439
Louis Houghton <louis@louishoughton.com>
2018-03-15 16:33:35 +0000

    Trying to get proxy working...

--- run-functional-tests.sh
+++ run-functional-tests.sh
@@ -2,7 +2,7 @@
 
 ADDITIONAL_COMPOSE_FILE=docker-compose.functional-tests.yml
 
-docker-compose -f ${ADDITIONAL_COMPOSE_FILE} up --no-color -d -e GRADLE_OPTS remote-webdriver
-docker-compose -f ${ADDITIONAL_COMPOSE_FILE} run -e GRADLE_OPTS node-demo-tests
+docker-compose -f ${ADDITIONAL_COMPOSE_FILE} up --no-color -d remote-webdriver
+docker-compose -f ${ADDITIONAL_COMPOSE_FILE} run node-demo-tests
 docker-compose -f ${ADDITIONAL_COMPOSE_FILE} down
 
