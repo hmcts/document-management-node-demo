ad57418e2abe250866d533add7ce6ec7127cf412
Louis Houghton <louis@louishoughton.com>
2018-03-16 10:03:42 +0000

    Update yarn before installing

--- run-functional-tests.sh
+++ run-functional-tests.sh
@@ -2,6 +2,7 @@
 
 ADDITIONAL_COMPOSE_FILE=docker-compose.functional-tests.yml
 
+docker-compose -f ${ADDITIONAL_COMPOSE_FILE} build node-demo-tests
 docker-compose -f ${ADDITIONAL_COMPOSE_FILE} up --no-color -d remote-webdriver
 docker-compose -f ${ADDITIONAL_COMPOSE_FILE} run node-demo-tests
 docker-compose -f ${ADDITIONAL_COMPOSE_FILE} down
