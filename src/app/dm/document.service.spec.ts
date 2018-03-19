4e1304cbf92e49060c7f4c49e8c49a10be6c849b
Alec Doran-Twyford <alec@alectronic.co>
2018-03-19 15:58:33 +0000

    fixed dm service

--- src/app/dm/document.service.spec.ts
+++ src/app/dm/document.service.spec.ts
@@ -2,12 +2,15 @@ import {DocumentStoreService} from './document-store.service';
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 import {AppConfig} from '../config/app.config';
 import {HttpClient} from '@angular/common/http';
-import {HttpTestingController} from '@angular/common/http/testing';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
+import {DocumentService} from '../utils/document.service';
+import {SessionService} from '../auth/session.service';
+import {CookieModule, CookieService} from 'ngx-cookie';
+import {WindowService} from '../utils/window.service';
 
 let httpMock: HttpTestingController;
 let appConfig: AppConfig;
-let component: DocumentStoreService;
-let fixture: ComponentFixture<DocumentStoreService>;
+let service: DocumentStoreService;
 
 const configObject = {
   'dm_store_app_local_endpoint': '/demproxy/dm/',
@@ -17,29 +20,27 @@ describe('DocumentStoreService', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [],
-      declarations: [DocumentStoreService],
-      providers: [HttpClient, AppConfig]
+      imports: [HttpClientTestingModule, CookieModule.forRoot()],
+      declarations: [],
+      providers: [AppConfig, DocumentStoreService, DocumentService, SessionService, WindowService]
     })
       .compileComponents();
   }));
 
   describe('Test ConvertUrlToProxy', () => {
     beforeEach(() => {
-      fixture = TestBed.createComponent(DocumentStoreService);
-      component = fixture.componentInstance;
+      service = TestBed.get(DocumentStoreService);
       httpMock = TestBed.get(HttpTestingController);
       appConfig = TestBed.get(AppConfig);
       appConfig.load();
       const configRequest = httpMock.expectOne('assets/config.json');
       configRequest.flush(configObject);
-      fixture.detectChanges();
     });
 
-    // it('ConvertUrlToProxy', () => {
-    //   expect(component.convertUrlToProxy('http://localhost:8080/testUrl'))
-    //     .toEqual('/demproxy/dm/testUrl');
-    // });
+    it('ConvertUrlToProxy', () => {
+      expect(service.convertUrlToProxy('http://localhost:8080/testUrl'))
+        .toEqual('/demproxy/dm/testUrl');
+    });
 
   });
 
