b3fe453db2f0725f24b1b54bd4b4faa93d1a2d7e
Phillip Denness <dennessphillip@gmail.com>
2019-01-08 17:25:24 +0000

    Remove sonarqube and jui readme

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,11 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
-import {Router, RouterModule} from '@angular/router';
 import {ConfigService} from './config.service';
-import {configMock} from './domain/services/mock/config.env.mock';
 import {RouterTestingModule} from '@angular/router/testing';
+
 describe('AppComponent', () => {
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [RouterTestingModule],
@@ -13,29 +13,22 @@ describe('AppComponent', () => {
         AppComponent
       ],
       providers: [
-        {
-          provide: ConfigService,
-          useValue: configMock
-        }
+          ConfigService
       ],
-      schemas:[CUSTOM_ELEMENTS_SCHEMA]
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
     }).compileComponents();
   }));
+
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const component = fixture.componentInstance;
     expect(component).toBeTruthy();
     expect(component).not.toBeNull();
   }));
+
   it(`should have as title 'app'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('JUI Web App');
+    expect(app.title).toEqual('Em Web Show');
   }));
-  // it('should render title in a h1 tag', async(() => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   fixture.detectChanges();
-  //   const compiled = fixture.debugElement.nativeElement;
-  //   // expect(compiled.querySelector('h1').textContent).toContain('JUI Web App');
-  // }));
 });
--- src/app/header/header.component.spec.ts
+++ src/app/header/header.component.spec.ts
@@ -1,10 +1,8 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 import {HeaderComponent} from './header.component';
-import {AuthService} from '../../../auth/auth.service';
-import {HmctsGlobalHeaderComponent} from '../../../hmcts/components/hmcts-global-header/hmcts-global-header.component';
-import {HmctsPrimaryNavigationComponent} from '../../../hmcts/components/hmcts-primary-navigation/hmcts-primary-navigation.component';
 import {RouterTestingModule} from '@angular/router/testing';
 import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import { AuthService } from '../auth/auth.service';
 
 describe('HeaderComponent', () => {
     let component: HeaderComponent;
@@ -13,7 +11,7 @@ describe('HeaderComponent', () => {
     beforeEach(async(() => {
         TestBed.configureTestingModule({
             imports: [RouterTestingModule],
-            declarations: [HeaderComponent, HmctsGlobalHeaderComponent, HmctsPrimaryNavigationComponent],
+            declarations: [HeaderComponent],
             schemas: [CUSTOM_ELEMENTS_SCHEMA],
             providers: [
                 {
@@ -29,16 +27,15 @@ describe('HeaderComponent', () => {
     beforeEach(() => {
         fixture = TestBed.createComponent(HeaderComponent);
         component = fixture.componentInstance;
+        component.navItems = [{
+            text: 'Demo',
+            href: '/',
+            active: true
+        }];
         fixture.detectChanges();
     });
 
     it('should create', () => {
         expect(component).toBeTruthy();
-    });
-
-    it('should test predefined data', () => {
-        fixture.detectChanges();
-        expect( component.navItems.length).toEqual(1);
-        expect( component.navigations.items.length).toEqual(1);
     });
 });
