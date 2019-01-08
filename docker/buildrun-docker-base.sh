069ef60f1bdd1c287303130baadbf840e001e332
Phillip Denness <dennessphillip@gmail.com>
2019-01-08 12:50:37 +0000

    old em docker compose files ignored

--- docker/buildrun-docker-base.sh
+++ docker/buildrun-docker-base.sh
@@ -4,16 +4,13 @@ clear;
 ${DIR}/../bin/fakeversion.sh
 
 docker-compose -f ${DIR}/compose/docker-compose-dm.yml \
--f ${DIR}/compose/docker-compose-em.yml \
 -f ${DIR}/compose/docker-compose-idam.yml \
 down
 
-docker-compose -f ${DIR}/compose/docker-compose-dm.yml \
--f ${DIR}/compose/docker-compose-em.yml \
--f ${DIR}/compose/docker-compose-idam.yml \
-pull
+# docker-compose -f ${DIR}/compose/docker-compose-dm.yml \
+# -f ${DIR}/compose/docker-compose-idam.yml \
+# pull
 
 docker-compose -f ${DIR}/compose/docker-compose-dm.yml \
--f ${DIR}/compose/docker-compose-em.yml \
 -f ${DIR}/compose/docker-compose-idam.yml \
 up --build
