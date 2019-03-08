bff7ae5d4c72961892f5b6a7621cb7906b451e5c
Linus Norton <linusnorton@gmail.com>
2019-03-08 12:09:28 +0000

    WIP

--- src/app/routing/pages/generic-page/doc-viewer/doc-viewer.component.ts
+++ src/app/routing/pages/generic-page/doc-viewer/doc-viewer.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./doc-viewer.component.scss']
 })
 export class DocViewerComponent implements OnInit {
+  url: string;
 
   constructor() { }
 
