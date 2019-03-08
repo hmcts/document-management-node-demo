36d2eb11eacdcfd84ebb89116ee987b9b2c2a2e6
Linus Norton <linusnorton@gmail.com>
2019-03-08 07:44:12 +0000

    remove axios, more unused code, fix build

--- api/index.ts
+++ api/index.ts
@@ -1,12 +1,12 @@
 import * as express from 'express';
-import * as authInterceptor from './lib/middleware/auth';
-import * as serviceTokenMiddleware from './lib/middleware/service-token';
 import { auth } from './controllers/auth';
 import { docAssemblyRoutes } from './services/dg-docassembly-api/dg-docassembly-api';
 import { emAnnoRoutes } from './services/em-anno-api/em-anno-api';
 import { emNpaRoutes } from './services/em-npa-api/em-npa-api';
 import { idamRoutes } from './services/idam-api/idam-api';
 import { serviceAuthRoutes } from './services/service-auth-provider-api/service-auth-provider-api';
+import { serviceTokenMiddleware } from './lib/middleware/service-token';
+import { authInterceptor } from './lib/middleware/auth';
 
 export function getRouter() {
     const router = express.Router();
